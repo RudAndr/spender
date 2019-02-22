5bed52b1e716b17fe9a20f3febee39de6c579778
a.rudenko <a.rudenko@controlpay.com>
2019-02-22 16:13:06 +0200

    add groovy support for testing units

---
+++ src/test/java/ua/boring/project/config/AppConfig.java
@@ -0,0 +1,4 @@
+package ua.boring.project.config;
+
+public class AppConfig {
+}
---
+++ src/test/java/ua/boring/project/config/BaseTestConfig.java
@@ -0,0 +1,30 @@
+package ua.boring.project.config;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import ua.boring.project.ProjectApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {AppConfig.class})
+@SpringBootTest(classes = ProjectApplication.class)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+public abstract class BaseTestConfig extends Assert {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
