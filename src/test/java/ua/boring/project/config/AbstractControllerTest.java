b6c393f1dd4508f65274f7a6d4e37ef28b060392
a.rudenko <a.rudenko@controlpay.com>
2019-02-25 16:09:22 +0200

    Controller tests

---
+++ src/test/java/ua/boring/project/config/AbstractControllerTest.java
@@ -0,0 +1,37 @@
+package ua.boring.project.config;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+
+public class AbstractControllerTest extends BaseTestConfig {
+
+    protected MockMvc mvc;
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+    protected <T> T mapFromJson(String json, Class<T> clazz) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+}
